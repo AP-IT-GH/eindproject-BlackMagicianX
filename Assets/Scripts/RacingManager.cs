1babdcd2a956f18c67096b0e4ab8f6a8b68916c0
BlackMagicianX <benalimarwanmbc@gmail.com>
2024-06-13 14:08:39 +0200

    Add New Race Track

--- Assets/Scripts/RacingManager.cs
+++ Assets/Scripts/RacingManager.cs
@@ -9,12 +9,15 @@ public class RacingManager : MonoBehaviour
     private bool lapComplete;
     private int lap;
     public KartAgent kartAgent;
+    public KartController kartController;
+    private float currentAccel;
 
     // Start is called before the first frame update
     void Start()
     {
         lapComplete = true;
         lap = 0;
+        currentAccel = kartController.acceleration;
     }
 
     void Update()
@@ -39,7 +42,19 @@ public class RacingManager : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Wall"))
         {
-            kartAgent.AddReward(-0.5f); // Adjust the penalty value as needed
+            kartAgent.AddReward(-1f); // Adjust the penalty value as needed
+            Debug.Log("bonk");
+        }
+        
+        if (collision.gameObject.CompareTag("OffRoad"))
+        {
+            kartAgent.AddReward(-1f); // Adjust the penalty value as needed
+            kartController.acceleration = 30f;
+            Debug.Log("Off Road");
+        }
+        else if (collision.gameObject.CompareTag("Road"))
+        {
+            kartController.acceleration = currentAccel;
         }
     }
 
