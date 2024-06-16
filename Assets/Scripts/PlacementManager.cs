2b76e0eab7413a6b5376f01de079bd26f810b48d
Kirittsu <keanusegers@hotmail.com>
2024-06-16 15:15:26 +0200

    End screen add

--- Assets/Scripts/PlacementManager.cs
+++ Assets/Scripts/PlacementManager.cs
@@ -11,6 +11,11 @@ public class PlacementManager : MonoBehaviour
 
     private int currentLap;
 
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
