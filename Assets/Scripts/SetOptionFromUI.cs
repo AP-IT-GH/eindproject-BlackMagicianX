507ec7080c929ccbffa9332ceb7635477334e893
BlackMagicianX <benalimarwanmbc@gmail.com>
2024-06-17 13:53:13 +0200

    Improve Menu screens; bug fixes

--- Assets/Scripts/SetOptionFromUI.cs
+++ Assets/Scripts/SetOptionFromUI.cs
@@ -14,20 +14,10 @@ public class SetOptionFromUI : MonoBehaviour
     private void Start()
     {
         volumeSlider.onValueChanged.AddListener(SetGlobalVolume);
-        turnDropdown.onValueChanged.AddListener(SetTurnPlayerPref);
-
-        if (PlayerPrefs.HasKey("turn"))
-            turnDropdown.SetValueWithoutNotify(PlayerPrefs.GetInt("turn"));
     }
 
     public void SetGlobalVolume(float value)
     {
         AudioListener.volume = value;
-    }
-
-    public void SetTurnPlayerPref(int value)
-    {
-        PlayerPrefs.SetInt("turn", value); 
-        turnTypeFromPlayerPref.ApplyPlayerPref();
     }
 }
